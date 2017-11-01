6617bf29f1834d296e76c4a258ec396ec142764b
Richard Cavell <richardcavell@mail.com>
2017-11-01 14:17:01 +0000

    Cosmetic changes to main2.s
      * Correct the comments - this one assembles no_error_strlen.s
      * Make _start the very first label encountered after the ORG
      * Use 4-space indents
    
    Signed-off-by: Richard Cavell <richardcavell@mail.com>

--- Test suite/main2.s
+++ Test suite/main2.s
@@ -2,17 +2,18 @@
 ; Version 1.2 (1 November 2017)
 ; https://github.com/richardcavell/6809-strlen
 ;
-; A very short file to assist in assembling strlen.s for the test suite
+; A very short file to assist in assembling no_error_strlen.s
+; for the test suite
 ;
 ; Written by Richard John Foster Cavell (c) 2017.
 ; Licensed to you under the MIT License.
 
-  ORG $3080                 ; Change this if you have <16K RAM
+    ORG $3080                 ; Change this if you have <16K RAM
 
-_main2
 _start
+_main2
 
-  INCLUDE "../no_error_strlen.s"
+    INCLUDE "../no_error_strlen.s"
 
 _end
-  END
+    END
