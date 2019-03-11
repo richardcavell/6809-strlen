3179e24c4b3ab5a7e7469a8418a7e096e634b090
Richard Cavell <richardcavell@mail.com>
2019-03-11 14:35:53 +0000

    Add a comment to helper2.s
      * For perfect clarity

--- Test suite/helper2.s
+++ Test suite/helper2.s
@@ -7,6 +7,8 @@
 ; Written by Richard John Foster Cavell (c) 2017, 2018, 2019.
 ; Licensed to you under the MIT License.
 ;
+; ($FFFF means hexadecimal FFFF)
+;
 ; Inputs:
 ;   Length of the string (0 to $FFFF)
 ;
